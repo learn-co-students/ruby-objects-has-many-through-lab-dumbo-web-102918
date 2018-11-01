9d966d58a45d2efd9c937269ab7b54b0ed515c8e
github username <raquel.sae.randall@gmail.com>
2018-11-01 18:56:33 -0400

    Done.

---
+++ lib/appointment.rb
@@ -0,0 +1,21 @@
+class Appointment
+  attr_accessor :name, :doctor, :date, :patient
+
+  @@all = []
+
+  def initialize(patient, date, doctor)
+    @date = date
+    @patient = patient
+    @doctor = doctor
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+
+
+
+
+end
