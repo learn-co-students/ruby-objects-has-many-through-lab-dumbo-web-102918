a28cfa662e118f0cc7ce5bb80d8a72ef6d762cb2
github username <github email address>
2018-11-02 20:01:36 -0400

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,33 @@
+class Artist
+
+  attr_accessor :name, :genre
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def new_song(name, genre)
+    song = Song.new(name, self, genre)
+    song.artist = self
+    song
+  end
+
+  def songs
+    this_artists_songs = Song.all.select do |song|
+      song.artist == self
+    end
+  end
+
+  def genres
+    songs.map do |song|
+      song.genre
+    end
+  end
+end
